241b0191fbaa8aa65d9449c4193bb7d29869e46d
Rimon Mahamud Rony <rimonronycste11@gmail.com>
2022-08-11 17:42:38 +0600

    front-end update-3

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -27,33 +27,45 @@
               <div class="row">
 
                 <div class="col">
-                  <div class="card" style="width: 18rem;">
+                  <div class="card" style="width: 15rem;">
                     <img src="{{asset('img/list.png') }}" height="180" width="180" class="card-img-top" alt="List of Employee">
                       <div class="card-body">
                         <h5 class="card-title">List of Employee</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p class="card-text"></p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                       </div>
                     </div>
                 </div>
 
                 <div class="col">
-                  <div class="card" style="width: 18rem;">
+                  <div class="card" style="width: 15rem;">
                     <img src="{{asset('img/add.png') }}" height="180" width="180" class="card-img-top" alt="Add  Employee">
                       <div class="card-body">
                         <h5 class="card-title">Add  Employee</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p class="card-text"></p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                       </div>
                     </div>
                 </div>
 
                 <div class="col">
-                  <div class="card" style="width: 18rem;">
+                  <div class="card" style="width: 15rem;">
                     <img src="{{asset('img/com.png') }}" height="180" width="180" class="card-img-top" alt="Add Company">
                       <div class="card-body">
                         <h5 class="card-title">Add Company</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p class="card-text"></p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                      </div>
+                    </div>
+                </div>
+
+             
+                <div class="col">
+                  <div class="card" style="width: 15rem;">
+                    <img src="{{asset('img/com.png') }}" height="180" width="180" class="card-img-top" alt="Add Company">
+                      <div class="card-body">
+                        <h5 class="card-title">List of Companies</h5>
+                        <p class="card-text"></p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                       </div>
                     </div>
@@ -61,6 +73,7 @@
 
               </div>
               <br>
+
             </div>
         </div>
     </div>
